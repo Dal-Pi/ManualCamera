e303c7769be5723c66ed626de9ba06505e764ede
Dal-Pi <kimsh707@naver.com>
2021-10-12 01:33:24 +0900

    impl camera basic
    following camera2Basic

--- app/src/main/java/com/kania/manualcamera/CameraFragment.kt
+++ app/src/main/java/com/kania/manualcamera/CameraFragment.kt
@@ -1,14 +1,41 @@
 package com.kania.manualcamera
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Camera
+import android.graphics.ImageFormat
+import android.hardware.camera2.*
+import android.media.Image
+import android.media.ImageReader
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.view.*
+import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.kania.manualcamera.databinding.FragmentCameraBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.Closeable
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.TimeoutException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM1 = "cameraId"
 private const val ARG_PARAM2 = "param2"
 
 /**
@@ -17,9 +44,45 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CameraFragment : Fragment() {
+
+
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    //TODO temp id
+    private var cameraId = "0"
+    private var imageFormat = ImageFormat.JPEG
+
+    private var _fragmentCameraBinding: FragmentCameraBinding? = null
+    private val fragmentCameraBinding get() = _fragmentCameraBinding!!
+
+    private val cameraManager: CameraManager by lazy {
+        val context = requireContext().applicationContext
+        context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    private val characteristics: CameraCharacteristics by lazy {
+        cameraManager.getCameraCharacteristics(cameraId)
+    }
+
+    private lateinit var imageReader: ImageReader
+
+    private val cameraThread = HandlerThread("CameraThread").apply { start() }
+
+    private val cameraHandler = Handler(cameraThread.looper)
+
+    private val imageReaderThread = HandlerThread("imageReaderThread").apply { start() }
+
+    private val imageReaderHandler = Handler(imageReaderThread.looper)
+
+    private lateinit var camera: CameraDevice
+
+    private lateinit var session: CameraCaptureSession
+
+    private lateinit var relativeOrientation: OrientationLiveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +95,275 @@ class CameraFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    ): View {
+        //fragement binding
+        _fragmentCameraBinding = FragmentCameraBinding.inflate(inflater, container, false)
+        return fragmentCameraBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentCameraBinding.viewFinder.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                val previewSize = getPreviewOutputSize(
+                    fragmentCameraBinding.viewFinder.display,
+                    characteristics,
+                    SurfaceHolder::class.java
+                )
+                Log.d(TAG, "View finder size: ${fragmentCameraBinding.viewFinder.width} x ${fragmentCameraBinding.viewFinder.height}")
+                Log.d(TAG, "Selected preview size: $previewSize")
+
+                fragmentCameraBinding.viewFinder.setAspectRatio(
+                    previewSize.width,
+                    previewSize.height
+                )
+
+                view.post { initializeCamera() }
+            }
+
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) = Unit
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) = Unit
+        })
+
+        //TODO about viewLifecycleOwner
+        relativeOrientation = OrientationLiveData(requireContext(), characteristics).apply {
+            observe(viewLifecycleOwner, Observer { orientation ->
+                Log.d(TAG, "Orientation changed: $orientation")
+            })
+        }
+    }
+
+    private fun initializeCamera() = lifecycleScope.launch(Dispatchers.Main) {
+        camera = openCamera(cameraManager, cameraId, cameraHandler)
+
+        val size = characteristics.get(
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            .getOutputSizes(imageFormat).maxByOrNull { it.height * it.width }!!
+        imageReader = ImageReader.newInstance(
+            size.width, size.height, imageFormat, IMAGE_BUFFER_SIZE)
+
+        val targets = listOf(fragmentCameraBinding.viewFinder.holder.surface, imageReader.surface)
+
+        session = createCaptureSession(camera, targets, cameraHandler)
+
+        val captureRequest = camera.createCaptureRequest(
+            CameraDevice.TEMPLATE_PREVIEW
+        ).apply {
+            addTarget(fragmentCameraBinding.viewFinder.holder.surface)
+        }
+
+        //TODO add CaptureCallback
+        session.setRepeatingRequest(captureRequest.build(), null, cameraHandler)
+
+        fragmentCameraBinding.captureButton.setOnClickListener {
+            it.isEnabled = false
+
+            //TODO about dispatchers
+            lifecycleScope.launch(Dispatchers.IO) {
+                takePhoto().use { result ->
+                    Log.d(TAG, "Result received: $result")
+
+                    val output = saveResult(result)
+                    Log.d(TAG, "Image saved: ${output.absolutePath}")
+
+                    if (output.extension == "jpg") {
+                        val exif = ExifInterface(output.absoluteFile)
+                        exif.setAttribute(
+                            ExifInterface.TAG_ORIENTATION, result.orientation.toString())
+                        exif.saveAttributes()
+                        Log.d(TAG, "EXIF metadata saved: ${output.absolutePath}")
+                    }
+
+                    lifecycleScope.launch(Dispatchers.Main) {
+                        //TODO view picture
+                    }
+                }
+
+                it.post { it.isEnabled = true }
+            }
+        }
+    }
+
+    //TODO permission
+    @SuppressLint("MissingPermission")
+    private suspend fun openCamera(
+        manager: CameraManager,
+        cameraId: String,
+        handler: Handler? = null) : CameraDevice = suspendCancellableCoroutine { cont ->
+        manager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) = cont.resume(camera)
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Log.w(TAG, "Camera $cameraId has been disconnected")
+                requireActivity().finish()
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                val msg = when (error) {
+                    ERROR_CAMERA_DEVICE -> "Fatal (device)"
+                    ERROR_CAMERA_DISABLED -> "Device policy"
+                    ERROR_CAMERA_IN_USE -> "Camera in use"
+                    ERROR_CAMERA_SERVICE -> "Fatal (service)"
+                    ERROR_MAX_CAMERAS_IN_USE -> "Maximum cameras in use"
+                    else -> "Unknown"
+                }
+                val exc = RuntimeException("Camera $cameraId error: ($error) $msg")
+                Log.e(TAG, exc.message, exc)
+                if (cont.isActive) cont.resumeWithException(exc)
+            }
+        }, handler)
+    }
+
+    private suspend fun createCaptureSession(
+        device: CameraDevice,
+        targets: List<Surface>,
+        handler: Handler? = null
+    ): CameraCaptureSession = suspendCoroutine { cont ->
+        //TODO deprecated
+        device.createCaptureSession(targets, object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) = cont.resume(session)
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {
+                val exc = RuntimeException("Camera ${device.id} session configuration failed")
+                Log.e(TAG, exc.message, exc)
+                cont.resumeWithException(exc)
+            }
+        }, handler)
+    }
+
+    private suspend fun takePhoto(): CombinedCaptureResult = suspendCoroutine { cont ->
+        while (imageReader.acquireNextImage() != null) {
+        }
+
+        val imageQueue = ArrayBlockingQueue<Image>(IMAGE_BUFFER_SIZE)
+        imageReader.setOnImageAvailableListener({ reader ->
+            val image = reader.acquireNextImage()
+        }, imageReaderHandler)
+
+        //TODO what is TEMPLATE_STILL_CAPTURE?
+        val captureResult = session.device.createCaptureRequest(
+            CameraDevice.TEMPLATE_STILL_CAPTURE).apply { addTarget(imageReader.surface) }
+        session.capture(captureResult.build(), object : CameraCaptureSession.CaptureCallback() {
+            override fun onCaptureStarted(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                timestamp: Long,
+                frameNumber: Long
+            ) {
+                super.onCaptureStarted(session, request, timestamp, frameNumber)
+                //TODO capture start action
+            }
+
+            override fun onCaptureCompleted(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                result: TotalCaptureResult
+            ) {
+                super.onCaptureCompleted(session, request, result)
+                val resultTimestamp = result.get(CaptureResult.SENSOR_TIMESTAMP)
+                Log.d(TAG, "Capture result received: $resultTimestamp")
+
+                val exc = TimeoutException("Image dequeuing took too long")
+                val timeoutRunnable = Runnable { cont.resumeWithException(exc) }
+                imageReaderHandler.postDelayed(timeoutRunnable, IMAGE_CAPTURE_TIMEOUT_MILLIS)
+
+                lifecycleScope.launch(cont.context) {
+                    while (true) {
+                        val image = imageQueue.take()
+                        //TODO what mean?
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                                image.format != ImageFormat.DEPTH_JPEG &&
+                                image.timestamp != resultTimestamp) continue
+                        Log.d(TAG, "Matching image dequeued: ${image.timestamp}")
+
+                        imageReaderHandler.removeCallbacks(timeoutRunnable)
+                        imageReader.setOnImageAvailableListener(null, null)
+
+                        while (imageQueue.size > 0) {
+                            imageQueue.take().close()
+                        }
+
+                        val rotation = relativeOrientation.value ?: 0
+                        val mirrored = characteristics.get(CameraCharacteristics.LENS_FACING) ==
+                                CameraCharacteristics.LENS_FACING_FRONT
+                        //TODO about orientation
+                        val exifOrientation = computeExifOrientation(rotation, mirrored)
+
+                        cont.resume(CombinedCaptureResult(
+                            image, result, exifOrientation, imageReader.imageFormat))
+                    }
+                }
+            }
+        }, cameraHandler)
+    }
+
+    private suspend fun saveResult(result: CombinedCaptureResult) : File = suspendCoroutine { cont ->
+        when (result.format) {
+            ImageFormat.JPEG, ImageFormat.DEPTH_JPEG -> {
+                val buffer = result.image.planes[0].buffer
+                val bytes = ByteArray(buffer.remaining()).apply { buffer.get(this) }
+                try {
+                    val output = createFile(requireContext(), "jpg")
+                    FileOutputStream(output).use { it.write(bytes) }
+                } catch (exc: IOException) {
+                    Log.e(TAG, "Unable to write JPEG image to file", exc)
+                    cont.resumeWithException(exc)
+                }
+            }
+            //TODO YUV image
+
+            //TODO ImageFormat.RAW_SENSOR
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        try {
+            camera.close()
+        } catch (exc: Throwable) {
+            Log.e(TAG, "Error closing camera", exc)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraThread.quitSafely()
+        imageReaderThread.quitSafely()
+    }
+
+    override fun onDestroyView() {
+        _fragmentCameraBinding = null
+        super.onDestroyView()
     }
 
     companion object {
+        private const val TAG = "CameraFragment"
+
+        private const val IMAGE_BUFFER_SIZE: Int = 3
+
+        private const val IMAGE_CAPTURE_TIMEOUT_MILLIS: Long = 5000
+
+        data class CombinedCaptureResult(
+            val image: Image,
+            val metadata: CaptureResult,
+            val orientation: Int,
+            val format: Int
+        ) : Closeable {
+            override fun close() = image.close()
+        }
+
+        private fun createFile(context: Context, extension: String): File {
+            val sdf = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_sss", Locale.KOREA)
+            return File(context.filesDir, "IMG_${sdf.format(Date())}.$extension")
+        }
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -55,5 +381,7 @@ class CameraFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+
     }
 }
